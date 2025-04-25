47f9a7aba22f5b84788e6157e9a9813630cf17ac
Always5436 <mohityadav98128@gmail.com>
2025-04-25 18:47:28 +0530

    file upload button improved

--- src/component/OCR/Imginpt.jsx
+++ src/component/OCR/Imginpt.jsx
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Tesseract from "tesseract.js";
 
-function Imginpt({message}) {
-    
+function Imginpt({ message }) {
+    const [loading, setLoading] = useState(false);
   function handleImage(e) {
     // console.log(e)
+    setLoading(true);
     const file = e.target.files[0];
-    if(file) {
-        // setLoading(true);
-        Tesseract.recognize(file, 'eng',{
-            logger: m=> console.log(m),
-        }).then(({data: {text}}) => {
-            const newMess = [...message.messages,{sender: "bot", text}]
-            message.setMessages(newMess);
-        });
+    if (file) {
+      // setLoading(true);
+      Tesseract.recognize(file, "eng", {
+        logger: (m) => console.log(m),
+      }).then(({ data: { text } }) => {
+        const newMess = [...message.messages, { sender: "bot", text }];
+        message.setMessages(newMess);
+        setLoading(false);
+      });
     }
-
+    setLoading(false);
   }
   return (
     <div>
@@ -27,7 +29,15 @@ function Imginpt({message}) {
         onChange={handleImage}
         className="hidden"
       />
-      <label htmlFor="imageInpt" className="mr-4 -ml-8 cursor-pointer bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded inline-blockcursor-pointer bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded inline-block">File</label>
+      {!loading && (<label
+        htmlFor="imageInpt"
+        className="mr-4 -ml-8 cursor-pointer bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded inline-blockcursor-pointer bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded inline-block "
+      >
+        File
+      </label>)}
+      {loading && (<button
+        disabled={"true"}
+       className="load mr-4 -ml-8 cursor-wait px-4 py-2 rounded bg-gray-500 hover:bg-gray-600">Wait</button>)}
     </div>
   );
 }
